b2e40392e1d305676e737f8213a9aecbb2a0043d
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2022-03-10 11:11:45 +0100

    V.01

--- api/database/index.ts
+++ api/database/index.ts
@@ -1,30 +1,3 @@
-// const { MongoClient } = require('mongodb');
-// main();
-// async function main() {
-//
-//     const uri = "mongodb+srv://admin:admin@cluster0.sn8tt.mongodb.net/cluster0?retryWrites=true&w=majority";
-//     const client = new MongoClient(uri);
-//     try {
-//         // Connect to the MongoDB cluster
-//         await client.connect();
-//
-//         // Make the appropriate DB calls
-//         await listDatabases(client);
-//
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         // Close the connection to the MongoDB cluster
-//         await client.close();
-//     }
-// }
-//
-// async function listDatabases(client : any){
-//     let databasesList = await client.db().admin().listDatabases();
-//     console.log("aa2");
-//     console.log("Databases:");
-//     databasesList.databases.forEach((db: { name: any; }) => console.log(` - ${db.name}`));
-// };
 
 import * as mongoDB from "mongodb";
 import * as dotenv from "dotenv";
